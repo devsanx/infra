5c1eff6a4eb8891247ea7f860dbf60203b93a550
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-02-19 07:33:17 +0000

    Restart codebase

--- Website/Web/Helpers/GetApiEndpoint.cs
+++
@@ -1,9 +0,0 @@
-﻿using Republic.Helpers;
-
-namespace Republic.Web.Helpers
-{
-	public class GetApiEndpoint
-	{
-		public static string GetProductionDefault() => $"https+http://localhost:7430/v{GetVersion.GetApiVersion()}";
-	}
-}
